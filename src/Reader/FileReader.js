d7fa8bf966f414610018d23f2713244c83ccbab6
Guilherme Quental <guilherme@grubbu.com>
2013-06-15 09:45:10 -0300

    Refactoring FileReader to add comments

--- src/Reader/FileReader.js
+++ src/Reader/FileReader.js
@@ -1,9 +1,22 @@
 var fs = require('fs');
 
+/**
+ * Class responsible for reading the contents of a file in the system
+ * and retrieving it's content
+ *
+ * @constructor
+ */
 var FileReader = function() {
 
 }
 
+/**
+ * Function that will retrieve the content of a choosen path and return it's
+ * contents through a callback
+ *
+ * @param {string} path
+ * @param {function} callback
+ */
 FileReader.prototype.read = function(path, callback) {
     fs.exists(path, function(exists) {
         if (exists) {
